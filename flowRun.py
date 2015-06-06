2a026f86c82a9e56d1af7326878b15203d0bd724
drobotnik <ykabaza@gmail.com>
2015-06-06 19:31:37 +0100

    fix

--- flowRun.py
+++ flowRun.py
@@ -1,65 +1,77 @@
-from flowmethods import *
+from flowMethods import *
 from flowlevels import *
 import copy
 import time
 
 
-
 loops = 0
 clock = time.time()
 start_clock = time.time()
-end_clock = time.time() - start_clock
+end_clock = time.time()
 
 
-def solve(flow_list, size, recursion_level):
+
+
+def solve(level, recursion_level):
+    #Code just to check performance
     global loops, start_clock, end_clock
     loops += 1
-    array = make_array(flow_list, size)
-    if loops % 100000 == 0:
+    if loops % 10000 == 0:
         end_clock, start_clock = time.time(), end_clock
-        print('{}s for last 0.1m loops. Total loops: {}m, Total time: {}mins'.format(round(end_clock - start_clock, 2),
-                                                                                round(loops / 1000000, 2),
+        print('{}s for last 0.01m loops. Total loops: {}m, Total time: {}mins'.format(round(end_clock - start_clock, 2),
+                                                                                round(loops / 100000, 2),
                                                                                 round((time.time() - clock) / 60, 2)))
-    full_board = all(all(row) for row in make_array(flow_list, size))
-    all_complete = all(flow.check_complete() for flow in flow_list)
-    move_options = rank_options(array, flow_list, size)
-    if all_complete and full_board:
-        return make_array(flow_list, size)
-    elif move_options:
-        for flow, moves in move_options:
+    #Actual code below
+    if level.complete():
+        return level.make_array()
+    elif level.make_choice():
+        for flow, moves in level.make_choice():
             for move in moves:
                 flow.add_dot(move)
-                possible_solution = solve(copy.deepcopy(flow_list), size, recursion_level + 1)
+                possible_solution = solve(copy.deepcopy(level), recursion_level + 1)
                 if type(possible_solution) == list:
                     return possible_solution
 
 
-def test(array, tsize):
-    print('Testing:')
+
+
+def test(array):
     global loops
     then = time.time()
-    seed = make_flows(array)
-    solution = solve(copy.deepcopy(seed), tsize, 0)
+    print('\n***** Testing:')
+    seed = Level(array)
+    print(seed, '\n')
+    solution = solve(copy.deepcopy(seed), 0)
+    outparams = (round(time.time() - then, 3), loops, seed.size)
+    outtext = '{} found in: {}, Loops: {}, Size: {}'
     if solution:
-        print('SOLUTION found in: {}, Loops: {}'.format(str(time.time() - then), str(loops)))
-        print_array(make_array(seed, tsize))
-        loops = 0
-        print_array(solution)
+
+        print(outtext.format('Solution!', *outparams))
+        for row in solution:
+            print(row)
     else:
-        print('UNSOLVABLE: {}, Loops: {}'.format(str(time.time() - then), str(loops)))
-        print_array(make_array(seed, tsize))
-        loops = 0
+        print(outtext.format('Unsolvable!', *outparams))
 
 
-test(l31, 3)
-test(l3x2, 3)
-test(l44, 4)
-test(l43, 4)
-test(l51, 5)
-test(l52, 5)
-test(l61, 6)
-test(l62, 6)
-test(l71, 7)
-test(l72, 7)
-test(l91, 9)
+# test(l31)
+#
+# test(l3x2)
+#
+# test(l44)
+#
+# test(l43)
+# test(l51)
+#
+# test(l52)
+#
+# test(l61)
 
+test(l62)  #still same bug where it fails to solve this one
+
+# test(l71)
+
+# test(l72)
+
+# test(l91)
+
+###
--- flowmethods.py
+++ flowmethods.py
@@ -1,101 +1,146 @@
+from flowlevels import *
 
-def make_flows(array):
+
+def get_colours_and_nodes(lvl):
     """
-    Creates a Flow object for each node in a given seed array.
-    :param array: list of strings array
-    :return: Flow objects
+    Gets list of colours and node locations.
+    Called once when making Flow objects
+    :param lvl: list of strings
+    :return:[[str, tup, tup],...]
     """
     out = []
     node_set = lambda x: set([c for row in x for c in row if c])
-    for node in node_set(array):
+    for node in node_set(lvl):
         helper = [node]
-        for i, r in enumerate(array):
+        for i, r in enumerate(lvl):
             for j, c in enumerate(r):
                 if c == node:
                     helper += [(i, j)]
         out += [helper]
-    return [Flow(colour, flow_path, end_point) for colour, flow_path, end_point in out]
+    return out
+
+
+def make_flows(lvl):
+    """
+    Called once when starting level.
+    makes a list of paired flows.
+    feeds into Level.__init__. Could possibly be merged when it joins
+    :param lvl:
+    :return:
+    """
+    flow_list = []
+    helper = []
+    for colo, start, end in get_colours_and_nodes(lvl):
+        helper += [[Flow(colo, start), Flow(colo, end)]]
+    for x, y in helper:
+        y.link(x)
+        x.link(y)
+        flow_list += [x, y]
+    return flow_list
 
 
 class Flow(object):
 
-        def __init__(self, colour, flow_path, finish):
-            self.colour = colour
-            self.flow_path = [flow_path]
-            self.finish = finish
+    def __init__(self, colour, path, pair=[]):
+        self.colour = colour
+        self.path = [path]
+        self.pair = pair
 
-        def add_dot(self, position):
-            self.flow_path.append(position)
+    def link(self, pair):
+        self.pair = pair
 
-        def check_complete(self):
-            """
-            returns True if Flow is complete
-            Does this by testing if end of flow path is next to self.finish"""
-            return 1 == sum([abs(i - j) for i, j in zip(self.flow_path[-1], self.finish)])
-
-        def find_empties(self, array, size=3):
-            """
-            return list of tuples giving empty spaces next to flow end. False if none
-            """
-            row, col = self.flow_path[-1]
-            adj_rows = [(row + adj, col) for adj in (-1, 1) if 0 <= row + adj < size and not array[row + adj][col]]
-            adj_cols = [(row, col + adj) for adj in (-1, 1) if 0 <= col + adj < size and not array[row][col + adj]]
+    def find_empties(self, lvl):
+        """
+        return list of tuples giving empty spaces next to flow end. False if none
+        """
+        if not self.complete():
+            row, col = self.path[-1]
+            adj_rows = [(row + adj, col) for adj in (-1, 1) if 0 <= row + adj < lvl.size and not lvl.make_array()[row + adj][col]]
+            adj_cols = [(row, col + adj) for adj in (-1, 1) if 0 <= col + adj < lvl.size and not lvl.make_array()[row][col + adj]]
             return adj_rows + adj_cols
-
-
-def make_array(flow_list, size=3):
-    """
-    Creates an array based on colours, paths and end points. Allows checking which spots are empty etc
-    :param flow_list: list of Flow objects
-    :param size: array side length
-    :return: list of strings array
-    """
-    out = [['' for _ in range(size)] for _ in range(size)]
-    for flow in flow_list:
-        for row, col in flow.flow_path:
-            out[row][col] = flow.colour
-        out[flow.finish[0]][flow.finish[1]] = flow.colour
-    return out
-
-
-def print_array(array):
-    """
-    Call to easily print progress of given array. Can use make_array to print updated array on the fly
-    :param array: list of strings array
-    :return: None
-    """
-    for row in array:
-        print(row)
-    print()
-
-
-def find_empties(array, flow_list, size=3):
-    """
-    Orders each flow by the number of possible moves. ie:
-    [[f1, [(1,0)],
-     [f2, [(1, 0), (0, 1)],
-     [f3, [(2, 3), (3, 2), (1, 2)]]
-    This is where optimisation of choosing position will happen. ATM it is purely by number of possible options.
-    :param flow_list: list of Flows
-    :param size: side length of level
-    :return: Returns a list of lists of flows and possible (empty) spots
-    """
-    empties = [[flow, flow.find_empties(array, size)] for flow in flow_list if flow.find_empties(array, size) and not flow.check_complete()]
-    return sorted(empties, key=lambda x: len(x[1]))
-
-
-def check_blocked(flow_list, array, size=3):
-    return any([len(flow.find_empties(array, size)) == 0 and not flow.check_complete() for flow in flow_list])
-
-
-def rank_options(array, flow_list, size):
-    flow_options = find_empties(array, flow_list, size)
-    # The following section seems to stop code from solving properly.
-    # after 50k loops and 20seconds. Need to think why
-    blocked = check_blocked(flow_list, array, size)     #<blocked also seems to cause it to fail at solving after 3.6m loops and 37 minutes
-    if blocked:
         return False
-    for flow, options in flow_options:
-        if len(options) == 1:
-            return [[flow, options]]
-    return flow_options
+
+    def add_dot(self, position):
+        self.path += [position]
+
+    def complete(self):
+        """
+        returns True if Flow is complete
+        Does this by testing if end of flow path is next to self.finish"""
+        return 1 == sum([abs(i - j) for i, j in zip(self.path[-1], self.pair.path[-1])])
+
+    def blocked(self, lvl):
+        """
+        returns True if Flow is blocked > not complete and no move options
+        Does this by testing if flow is incomplete and has no options on one or both ends """
+        # l3x2 should break immediately because f1 is blocked in (0, 0)
+        return not self.find_empties(lvl) and not self.complete()
+
+
+class Level(object):
+
+    def __init__(self, lvl):
+        # maybe make it take in flow_list as a parameter so make_flows doesnt need to be called every time we init lvl
+        self.flow_list = make_flows(lvl)
+        self.size = len(lvl)
+
+    def __str__(self):
+        """
+        returns formatted level
+        """
+        return '\n'.join(str([(c or ' ') for c in r]) for r in self.make_array())
+
+    def make_array(self):
+        out = [['' for _ in range(self.size)] for _ in range(self.size)]
+        for flow in self.flow_list:
+            for row, col in flow.path:
+                out[row][col] = flow.colour
+        return out
+
+    def blocked(self):
+        """
+        returns True if any of the flows in the level are blocked
+        :return: bool
+        """
+        return any(f.blocked(self) for f in self.flow_list)
+
+    def make_choice(self):
+        """
+        Takes move_options to give False, 1 or more options.
+        Important to return ALL options unless
+        :return:
+        """
+        options = self.move_options()
+        #for flow, option in options:
+            #if len(option) == 1:
+            #    return [[flow, option]]
+        #      if two flow ends are against the edge of the map, with none blocking
+            # fill in the spots between
+            # How do I get it to place all these moves? do i need to bypass add_dot
+            # Can I memoise the fact that I have tried this? To prevent infinite loop.
+
+            # If a loop end is next to a corner, fill that spot, return only that dot move
+
+        return options
+
+    def move_options(self):
+        """Only enters if no nodes are blocked and none have 1 option only.
+        ie - multiple Flows have multiple options
+        #need to remember to return all options if introducing preemptive moves
+        """
+        #l3x1 should not be looping since n(0,0) is blocked
+        if not self.blocked():
+            flow_options = [[f, f.find_empties(self)] for f in self.flow_list if f.find_empties(self)]
+            flow_options = sorted(flow_options, key=lambda x: len(x[1]))
+            return flow_options
+        return []
+
+    def complete(self):
+        map_full = all(all(row) for row in self.make_array())
+        flows_done = all(f.complete() for f in self.flow_list)
+        return flows_done and map_full
+
+
+####
+
+test = Level(l31)
