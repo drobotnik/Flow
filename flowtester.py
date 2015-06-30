455ca1b09dc57d4f63514055d9e189a2f3c9e0fc
drobotnik <ykabaza@gmail.com>
2015-06-30 14:06:26 +0100

    Revert 770c4a5..fc95658
    This rolls back to commit 770c4a5b4cd0a44721afd0ad8d54dc63205a9bf2.

--- flowtester.py
+++ flowtester.py
@@ -15,31 +15,43 @@ two_flows_blank = [[[], []],
 
 one_flow_blank = [[[], []]]
 
-t = Level(t2l41)
+l = [[n, Level(x)] for n, x in enumerate([t1l31, t1l32, t1l33, t1l34,  # 32 and 33 should be false (0, 2) is blocked
+                                          t2l31,
+                                          t2l41, t2l42, t2l43, t2l44,
+                                          t2l51, t2l52, t2l53, t2l54])]
+
+# t2l31 - needs to throw a trigger somewhere along the way
+# t2l54 - How do I handle this? Maybe Flow ends count as being in two areas if possible.
+# In this case, B will be blocked whichever way A moves.
+# This should be coming up as False somewhere.
+
+# t2l44 - How do I handle this?. Need to check both pairs are in an area
+
+for n, level in l:
+    print(n)
+    print(level)
+    # print(len(level.ends()) == len(level), level.size ** 2 == len(level.filled()) + len(level.empties()))
+    if False: # not any([level.blocked(), level.dammed(), level.separated_flows(), level.cornered(), True]):
+        print('OK')
+    else:
+        tests = [['Dammed areas', level.dammed()],
+                 ['Flows seperated', level.separated_flows()],
+                 ['Blocked flows', level.blocked()],
+                 ['Cornered', level.cornered()],
+                 ['Types', level.find_adjacent((2, 2), diag=True)]]
+        for name, test in tests:
+            if test:
+                print(name, test)
+    # area_finder = level.area_finder()
+    # print('Number of areas: {}'.format(len(area_finder)))
+    # for na, area in enumerate(area_finder):
+    # print("Area {}: {}".format(na, area))
+    print()
 
 
-def area_finder(level_obj):
-    filled = []
-    for flow in level_obj:
-        filled += flow.path
 
-    empties = []
-    for spot in product(range(level_obj.size), repeat=2):
-        if spot not in filled:
-            empties += [spot]
-
-    areas = []
-    while empties:
-        area = [empties.pop()]
-        for spot in area:
-            for adj in level_obj.find_adjacent(spot):
-                try:
-                    area += [empties.pop(empties.index(adj))]
-                except ValueError:
-                    pass
-        areas += [area]
-    return areas
-
-
-
-
+print(level)
+print('x')
+print(level.make_array())
+print(level.make_array()[0][3])
+print(';')
